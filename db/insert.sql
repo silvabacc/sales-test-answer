b1138785f1de0192f9ba12ee74e6e15718d8707f
silvabacc <47926269+silvabacc@users.noreply.github.com>
2022-12-09 13:05:51 +0000

    2 files modified

--- db/insert.sql
+++ db/insert.sql
@@ -1,12 +1,3 @@
-insert into
-  total_sales (model_id, revenue, units)
-values
-  (94, 72000.12, 168),
-  (27, 444795.32, 946),
-  (100, 90449.76, 893),
-  (45, 81570.34, 537),
-  (66, 151121.98, 48);
-
 insert into 
   phones (model_id, model_name, brand, price, country, period)
 values
@@ -19,6 +10,17 @@ values
   (45, 'Galaxy', 'Samsung', 600, 'DE', 2),
   (66, 'iPhone 14', 'Apple', 599, 'GB', 2),
   (66, 'iPhone 14', 'Apple', 699, 'DE', 2);
+
+insert into
+  total_sales (model_id, revenue, units)
+values
+  (94, 72000.12, 168),
+  (27, 444795.32, 946),
+  (100, 90449.76, 893),
+  (45, 81570.34, 537),
+  (66, 151121.98, 48);
+
+
   
 -- Write a query that aggregates all sales of smartphones in GB
 -- Extend this query to involve all smartphones that are only iPhone 14
